8d75545a760c076280ef3aa1972b050de4593115
MeReda <medredaben14@gmail.com>
2023-09-28 23:37:42 +0100

    init

---
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -0,0 +1,29 @@
+import Editor from '@/components/Editor'
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntry = async (id: any) => {
+  const user = await getUserByClerkID()
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId: user.id,
+      id,
+    },
+    include: {
+      analysis: true,
+    },
+  })
+  return entry
+}
+
+const EntryPage = async ({ params }: any) => {
+  const entry = await getEntry(params.id)
+
+  return (
+    <div className="h-full w-full">
+      <Editor entry={entry} />
+    </div>
+  )
+}
+
+export default EntryPage
